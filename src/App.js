a9de0801559988ace425fa76d86de43dab7eae5b
Attit11 <sharmaattit09@gmail.com>
2020-05-09 08:21:50 +0545

    Coin flipper completed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CoinContainer from "./CoinContainer";
+
+import './App.css';
+
+class App extends React.Component{
+  render()
+  {
+    return(
+      <div className="App">
+        <CoinContainer />
+      </div>
+    )
+  }
+}
+export default App;
